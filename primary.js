d631254c82ddcc951e8c893251c569d87e7af502
Dylan Sieren <dylan.sieren01@gmail.com>
2019-01-22 18:25:34 -0600

    added the slick slider library, just testing it out to see if i can get it to work. If not I will move to using pointer events, I might even move to pointer events just to learn about them

--- primary.js
+++ primary.js
@@ -1,26 +1,4 @@
-let slideIndex = 1;
-const previousButton = document.querySelector(".previous");
-const nextButton = document.querySelector(".next");
-previousButton.addEventListener("click", () => moveContent(-1));
-nextButton.addEventListener("click", () => moveContent(1));
-showContent(slideIndex);
-
-
-
-// Testimonial Slider Functionality
-function moveContent(slideIndexValue) {
-    showContent((slideIndex += slideIndexValue));
-  }
-  function showContent(slideIndexValue) {
-    const slideContainer = document.getElementsByClassName("slide-container");
-    if (slideIndexValue > slideContainer.length) {
-      slideIndex = 1;
-    }
-    if (slideIndexValue < 1) {
-      slideIndex = slideContainer.length;
-    }
-    for (let value of slideContainer) {
-      value.style.display = "none";
-    }
-    slideContainer[slideIndex - 1].style.display = "flex";
-  }
+$(document).ready(function(){
+  $('.single-item').slick({
+  });
+});
