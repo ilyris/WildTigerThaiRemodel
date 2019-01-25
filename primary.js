b600ff4bd8ebb40fe32a5677eb6dc15f981958b5
Dylan Sieren <dylan.sieren01@gmail.com>
2019-01-25 00:31:04 -0600

    added in scrollable navigation changes logic so when we get that new logo as the user scrolls the logo will appear to the left instead of the text.. that's what were going for might look ugly, also set up slide-down transition for the product card hover

--- primary.js
+++ primary.js
@@ -1,3 +1,22 @@
+
+window.onscroll = () => {
+  const logo = document.querySelector('.logo');
+  const navUl = document.querySelector('.main-ul');
+
+  navUl.classList.toggle(
+    'scrolled-ul',  
+    navUl.scrollTop  > 25 ||  document.documentElement.scrollTop > 25);
+
+    logo.classList.toggle(
+      'scrolled-logo',  
+      navUl.scrollTop  > 25 ||  document.documentElement.scrollTop > 25);
+}
+
+
+
+
+/* Initalize Slick Slider */
+
 $(document).ready(function(){
   $('.single-item').slick({
   });
