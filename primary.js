ba03becabe6a18db893155ef14f73e9735b4082b
Dylan Sieren <dylan.sieren01@gmail.com>
2019-01-26 13:12:16 -0600

    removed the newly created logo from the menu, didnt work with the design well

--- primary.js
+++ primary.js
@@ -1,17 +1,4 @@
 
-window.onscroll = () => {
-  const logo = document.querySelector('.logo');
-  const navUl = document.querySelector('.main-ul');
-
-  navUl.classList.toggle(
-    'scrolled-ul',  
-    navUl.scrollTop  > 25 ||  document.documentElement.scrollTop > 25);
-
-    logo.classList.toggle(
-      'scrolled-logo',  
-      navUl.scrollTop  > 25 ||  document.documentElement.scrollTop > 25);
-}
-
 
 
 
